8be37e077f5f578e97b477e9f9a2c9ff9c3a59e6
Alishah Safdari <alishahsafdari@gmail.com>
2023-06-14 19:55:17 +0430

    Add all files

---
+++ src/components/fetchedUesrs.js
@@ -0,0 +1,29 @@
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getUserItems } from '../store/users/usersSlice';
+
+const UserList = () => {
+    const {userItem, isLoading, error} = useSelector((state) => state.users);
+    console.log(userItem);
+    console.log(isLoading);
+    console.log(error);
+    const dispatch = useDispatch();
+    useEffect(()=>{
+        dispatch(getUserItems())
+    },[]);
+
+    return(
+        <div>
+      <h1>Users</h1>
+      {isLoading && <div>Loading...</div>}
+      {error && <div>Error:{error}</div>}
+      {!isLoading&&userItem.length ? (
+        <ul>
+          {userItem.map(user => (
+           <li key={user.name.first}>{user.name.last}, {user.name.last}</li>))}
+        </ul>) : null}
+        </div>
+    );
+};
+
+export default UserList;
